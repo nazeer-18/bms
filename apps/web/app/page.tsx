d7bec8d215571a2d079e946b35d65cd95bf06ead
nazeer-18 <nazeer18@NAZEER-PC.>
2025-06-22 08:36:27 +0000

    tested cd

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,7 +4,7 @@ export default async function Home() {
   const user = await client.user.findFirst();
   return (
     <div>
-      <h1>Welcome,nazeer {user?.userName}!</h1>
+      <h1>Welcome,nazeer checking deployment {user?.userName}!</h1>
       {user?.password}
     </div>
   );
